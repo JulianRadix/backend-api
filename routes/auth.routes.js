564574dc98e21166328d171c2258dbcc05bcf445
Piet-Jan Klaas <joradix@st.noorderpoort.nl>
2025-03-09 22:57:19 +0100

    Update files - automated commit

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,10 +2,10 @@ import { Router } from "express";
 
 const authRouter = Router();
 
-authRouter.post("/sign-up", (req, res) => res.end({title: 'Sign up'}));
+authRouter.post("/sign-up", (req, res) => res.send({title: 'Sign up'}));
 
-authRouter.post("/sign-in", (req, res) => res.end({title: 'Sign in'}));
+authRouter.post("/sign-in", (req, res) => res.send({title: 'Sign in'}));
 
-authRouter.post("/sign-out", (req, res) => res.end({title: 'Sign out'}));
+authRouter.post("/sign-out", (req, res) => res.send({title: 'Sign out'}));
 
 export default authRouter;
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,14 +2,14 @@ import { Router } from "express";
 
 const userRouter = Router();
 
-userRouter.get("/", (req, res) => res.end({title: 'GET all users'}));
+userRouter.get("/", (req, res) => res.send({title: 'GET all users'}));
 
-userRouter.get("/:id", (req, res) => res.end({title: 'GET user details'}));
+userRouter.get("/:id", (req, res) => res.send({title: 'GET user details'}));
 
-userRouter.post("/", (req, res) => res.end({title: 'CREATE new user'}));
+userRouter.post("/", (req, res) => res.send({title: 'CREATE new user'}));
 
-userRouter.put("/:id", (req, res) => res.end({title: 'UPDATE user'}));
+userRouter.put("/:id", (req, res) => res.send({title: 'UPDATE user'}));
 
-userRouter.delete("/:id", (req, res) => res.end({title: 'DELETE user'}));
+userRouter.delete("/:id", (req, res) => res.send({title: 'DELETE user'}));
 
 export default userRouter;
