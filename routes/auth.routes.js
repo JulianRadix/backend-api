52cdd22ab53c7fb1d170a47950387a3e77bc4998
Piet-Jan Klaas <joradix@st.noorderpoort.nl>
2025-03-09 22:54:12 +0100

    Update files - automated commit

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,5 +3,9 @@ import { Router } from "express";
 const authRouter = Router();
 
 authRouter.post("/sign-up", (req, res) => res.end({title: 'Sign up'}));
+
 authRouter.post("/sign-in", (req, res) => res.end({title: 'Sign in'}));
+
 authRouter.post("/sign-out", (req, res) => res.end({title: 'Sign out'}));
+
+export default authRouter;
--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -4,4 +4,18 @@ const subscriptionRouter = Router();
 
 subscriptionRouter.get("/", (req, res) => res.end({title: 'GET all subscriptions'}));
 
+subscriptionRouter.get("/:id", (req, res) => res.end({title: 'GET subscription details'}));
+
+subscriptionRouter.post("/", (req, res) => res.end({title: 'CREATE subscription'}));
+
+subscriptionRouter.put("/:id", (req, res) => res.end({title: 'UPDATE subscription'}));
+
+subscriptionRouter.delete("/:id", (req, res) => res.end({title: 'DELETE subscription'}));
+
+subscriptionRouter.get("/user/:id", (req, res) => res.end({title: 'GET all user subscriptions'}));
+
+subscriptionRouter.put("/:id/cancel", (req, res) => res.end({title: 'CANCEL subscription'}));
+
+subscriptionRouter.get("/upcoming-renewals", (req, res) => res.end({title: 'GET upcoming renewals'}));
+
 export default subscriptionRouter;
